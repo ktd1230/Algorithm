640aa611a36044150e53424276dd7d5062b97351
김태동 <ktd1230@naver.com>
2020-05-08 23:33:21 +0900

    boj 1120 문자열

---
+++ 백준/문자열.java
@@ -0,0 +1,30 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 문자열 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		String A = st.nextToken();
+		String B = st.nextToken();
+		
+		int min = Integer.MAX_VALUE;
+		for (int i = 0; i < B.length() - A.length() + 1; i++) {
+			int cnt = 0;
+			for (int j = 0; j < A.length(); j++) {
+				if(B.charAt(i + j) != A.charAt(j))
+					cnt++;
+			}
+			min = Math.min(min, cnt);
+		}
+		
+		System.out.println(min);
+	}
+	
+}
