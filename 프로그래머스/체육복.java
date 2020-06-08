75ce8bb2fb632b8e275c47708d03759726d47317
ktd1230 <ktd1230@naver.com>
2020-06-08 23:47:08 +0900

    programmers 탐욕법(Greedy) 체육복

---
+++ 프로그래머스/체육복.java
@@ -0,0 +1,33 @@
+package 프로그래머스;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class 체육복 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		int[] lost = {3};
+		int[] reserve = {1};
+		System.out.println(solution(n, lost, reserve));
+	}
+	
+	public static int solution(int n, int[] lost, int[] reserve) {
+		int answer = 0;
+		ArrayList<Integer> list = new ArrayList<>();
+		for (int i = 0; i < lost.length; i++) {
+			list.add(lost[i]);
+		}
+		Collections.sort(list);
+		for (int i = 0; i < reserve.length; i++) {
+			for (int j = 0; j < list.size(); j++) {
+				if(reserve[i] - 1 == list.get(j) || reserve[i] + 1 == list.get(j)) {
+					list.remove(j);
+					break;
+				}
+			}
+		}
+		answer = n - list.size();
+		return answer;
+	}
+}
