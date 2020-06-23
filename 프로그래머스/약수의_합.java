f54454362b5ac7dd33cdf5098626ae1b370cf054
ktd1230 <ktd1230@naver.com>
2020-06-23 23:43:55 +0900

    programmers 연습문제 약수의 합

---
+++ 프로그래머스/약수의_합.java
@@ -0,0 +1,18 @@
+package 프로그래머스;
+
+public class 약수의_합 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		System.out.println(solution(n));
+	}
+	
+	public static int solution(int n) {
+		int answer = 0;
+		for (int i = 1; i <= n; i++) {
+			if(n % i == 0)
+				answer += i;
+		}
+		return answer;
+	}
+}
