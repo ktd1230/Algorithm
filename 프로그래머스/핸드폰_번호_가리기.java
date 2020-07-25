0d9a8d2a431cf166b93cd94a376774d6f211fef7
ktd1230 <ktd1230@naver.com>
2020-07-25 23:30:41 +0900

    programmers 연습문제 핸드폰 번호 가리기

---
+++ 프로그래머스/핸드폰_번호_가리기.java
@@ -0,0 +1,19 @@
+package 프로그래머스;
+
+public class 핸드폰_번호_가리기 {
+
+	public static void main(String[] args) {
+		String phone_number = "01033334444"
+				+ "";
+		System.out.println(solution(phone_number));
+	}
+	
+	public static String solution(String phone_number) {
+		String answer = "";
+        for(int i = 0; i < phone_number.length() - 4; i++) {
+            answer += "*";
+        }
+        answer += phone_number.substring(phone_number.length() - 4);
+        return answer;
+	}
+}
