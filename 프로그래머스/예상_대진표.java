1d1469c1e7e778d0589739e209e95b3c693ba8be
ktd1230 <ktd1230@naver.com>
2020-08-20 23:52:54 +0900

    programmers 2017 팁스타운 예상 대진표

---
+++ 프로그래머스/예상_대진표.java
@@ -0,0 +1,24 @@
+package 프로그래머스;
+
+public class 예상_대진표 {
+	
+	public static void main(String[] args) {
+		int n = 8;
+		int a = 4;
+		int b = 7;
+		System.out.println(solution(n, a, b));
+	}
+
+	public static int solution(int n, int a, int b) {
+		int answer = 0;
+		a = a % 2 == 0 ? a : a + 1;
+		b = b % 2 == 0 ? b : b + 1;
+		while(Math.abs(a - b) != 1) {
+			a /= 2;
+			b /= 2;
+			answer++;
+		}
+		answer++;
+		return answer;
+	}
+}
