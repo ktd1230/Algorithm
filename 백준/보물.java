f6e4d15b992d702a2f7059ef1bfe6798d09ba11b
ktd1230 <ktd1230@naver.com>
2020-08-28 23:50:32 +0900

    boj 1026 보물

---
+++ 백준/보물.java
@@ -0,0 +1,47 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class 보물 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int[] A = new int[N];
+		Integer[] B = new Integer[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			B[i] = Integer.parseInt(st.nextToken());
+		}
+
+		Arrays.sort(A);
+		Arrays.sort(B, new Comparator<Integer>() {
+
+			@Override
+			public int compare(Integer o1, Integer o2) {
+				// TODO Auto-generated method stub
+				return o2 - o1;
+			}
+		});
+		int idx = 0;
+		int sum = 0;
+		for (int i = 0; i < N; i++) {
+			sum += A[i] * B[idx++];
+		}
+
+		System.out.println(sum);
+	}
+
+}
