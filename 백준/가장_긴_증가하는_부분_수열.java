d252e2691059ec82c7981e0483620fa84a98f7ca
ktd1230 <ktd1230@naver.com>
2020-10-30 23:24:16 +0900

    boj 11053 가장 긴 증가하는 부분 수열

---
+++ 백준/가장_긴_증가하는_부분_수열.java
@@ -0,0 +1,32 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 가장_긴_증가하는_부분_수열 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int[] dp = new int[N];
+		int max = 0;
+		for (int i = 0; i < N; i++) {
+			dp[i] = 1;
+			for (int j = 0; j < i; j++) {
+				if(arr[i] > arr[j] && dp[i] <= dp[j])
+					dp[i] = dp[j] + 1;
+			}
+			max = Math.max(max, dp[i]);
+		}
+		System.out.println(max);
+	}
+
+}
