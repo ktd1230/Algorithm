d0c0e50420a96663c560119de2a7973a8edfffc3
ktd1230 <ktd1230@naver.com>
2020-11-02 23:26:19 +0900

    boj 1912 연속합

---
+++ 백준/연속합.java
@@ -0,0 +1,29 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 연속합 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int[] arr = new int[n];
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		int[] dp = new int[n];
+		dp[0] = arr[0];
+		int answer = dp[0];
+		for (int i = 1; i < n; i++) {
+			dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+			answer = Math.max(answer, dp[i]);
+		}
+		
+		System.out.println(answer);
+	}
+
+}
